87c0a4731f57cc56faa59b03bd890b77e071cf62
StanislavMironenko <98009389+StanislavMironenko@users.noreply.github.com>
2022-08-08 16:02:00 +0300

    Task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+}
 
+refs.startBtn.addEventListener('click', start);
+refs.stopBtn.addEventListener('click',stopColor)
+
+
+let isActive = false;
+let intervalID = null;
+   function  start()  {
+                
+         intervalID = setInterval(() => {
+            const color = getRandomHexColor();
+             document.body.style.backgroundColor = color;
+             refs.startBtn.setAttribute('disabled', true);
+          
+        }
+        ,1000)        
+};
+    
+ function    stopColor() {
+       clearInterval(intervalID)          
+       refs.startBtn.removeAttribute('disabled');
+    }
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
