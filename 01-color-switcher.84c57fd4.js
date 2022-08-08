45c7ef360e9d11a60fb0f336cd2f7273a10f5092
StanislavMironenko <98009389+StanislavMironenko@users.noreply.github.com>
2022-08-08 18:20:47 +0000

    Deploying to gh-pages from @ StanislavMironenko/goit-js-hw-09@766f73de63ecc0b5c4ab2cb89d51bcad528e86ae 🚀

---
+++ 01-color-switcher.84c57fd4.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(){e=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}),1e3),t.startBtn.setAttribute("disabled",!0)})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.removeAttribute("disabled")}));let e=null;
+//# sourceMappingURL=01-color-switcher.84c57fd4.js.map
